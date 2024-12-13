649cfc80c2f89a171913f6a718c2c0cba934eb7e
stdStudent <78802930+stdStudent@users.noreply.github.com>
2024-12-13 15:06:46 +0500

    [ADD]: Convention for Either type depending on PeType.

--- src/main/kotlin/headers/pe/type/PeType.kt
+++ src/main/kotlin/headers/pe/type/PeType.kt
@@ -1,5 +1,6 @@
 package std.student.headers.pe.type
 
+import arrow.core.Either
 import std.student.conventions.DWord
 import std.student.conventions.QWord
 import std.student.conventions.Word
@@ -17,5 +18,12 @@ sealed class PeType: PeTypeCharacteristic {
         const val PE32_MAGIC: Word = 0x10b
         const val PE32_PLUS_MAGIC: Word = 0x20b
         const val ROM_IMAGE: Word = 0x107 // Might never be supported
+
+        /**
+         * Convention: in data: Either<A, B>, A if of type Pe32, B if of type Pe32Plus.
+         * @return true if the data is of the correct type, false otherwise.
+         */
+        fun Either<*, *>.isCorrectType(peType: PeType): Boolean =
+            (this.isLeft() && peType is Pe32) || (this.isRight() && peType is Pe32Plus)
     }
 }
