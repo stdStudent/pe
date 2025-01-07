d72255c683389d10a044b2dc42a9dd3f8e17ddb9
stdStudent <78802930+stdStudent@users.noreply.github.com>
2025-01-07 22:31:09 +0500

    [UPD]: TypeAliases: allow to input string with quotes and bytes as ASCII.

--- src/main/kotlin/conventions/TypeAliases.kt
+++ src/main/kotlin/conventions/TypeAliases.kt
@@ -46,6 +46,29 @@ inline var ByteBuffer.qword: QWord
 // ----------------------
 // Reflection type traits
 
+private fun String.isSingleQuoted(): Boolean = this.startsWith("'") && this.endsWith("'")
+private fun String.isDoubleQuoted(): Boolean = this.startsWith("\"") && this.endsWith("\"")
+
+/**
+ * Example: 'MZ' to "0x5A4D"
+ */
+private fun String.getNumericRepresentation(): String =
+    this
+        .substring(1, this.length - 1)
+        .map { it.code.toString(16).padStart(2, '0') }
+        .reversed()
+        .joinToString("")
+        .let { "0x$it" }
+
+/**
+ * Example: "abc" to "97 98 99"
+ */
+private fun String.getArrayRepresentation(): String =
+    this
+        .substring(1, this.length - 1)
+        .map { it.code.toString() }
+        .joinToString(" ")
+
 private fun String.convert(): Long = when {
     this.startsWith("0x") -> {
         val value = this.substring(2)
@@ -61,30 +84,36 @@ private fun String.convert(): Long = when {
 }
 
 fun String.getInstance(arg: String): Any {
+    val value = when {
+        arg.isSingleQuoted() -> arg.getNumericRepresentation()
+        arg.isDoubleQuoted() -> arg.getArrayRepresentation()
+        else -> arg
+    }
+
     return when (this) {
         // Primitive types
-        "Byte" -> arg.convert().toByte()
-        "Short" -> arg.convert().toShort()
-        "Int" -> arg.convert().toInt()
-        "Long" -> arg.convert().toLong()
+        "Byte" -> value.convert().toByte()
+        "Short" -> value.convert().toShort()
+        "Int" -> value.convert().toInt()
+        "Long" -> value.convert().toLong()
 
         // Collection types
-        "ByteArray" -> arg.split(" ").map { it.convert().toByte() }.toByteArray()
-        "List<Byte>" -> arg.split(" ").map { it.convert().toByte() }
-        "List<Short>" -> arg.split(" ").map { it.convert().toShort() }
-        "List<Int>" -> arg.split(" ").map { it.convert().toInt() }
-        "List<Long>" -> arg.split(" ").map { it.convert().toLong() }
+        "ByteArray" -> value.split(" ").map { it.convert().toByte() }.toByteArray()
+        "List<Byte>" -> value.split(" ").map { it.convert().toByte() }
+        "List<Short>" -> value.split(" ").map { it.convert().toShort() }
+        "List<Int>" -> value.split(" ").map { it.convert().toInt() }
+        "List<Long>" -> value.split(" ").map { it.convert().toLong() }
 
         // Arrow types
-        "Left<Byte>" -> arg.convert().toByte().left()
-        "Left<Short>" -> arg.convert().toShort().left()
-        "Left<Int>" -> arg.convert().toInt().left()
-        "Left<Long>" -> arg.convert().toLong().left()
+        "Left<Byte>" -> value.convert().toByte().left()
+        "Left<Short>" -> value.convert().toShort().left()
+        "Left<Int>" -> value.convert().toInt().left()
+        "Left<Long>" -> value.convert().toLong().left()
 
-        "Right<Byte>" -> arg.convert().toByte().right()
-        "Right<Short>" -> arg.convert().toShort().right()
-        "Right<Int>" -> arg.convert().toInt().right()
-        "Right<Long>" -> arg.convert().toLong().right()
+        "Right<Byte>" -> value.convert().toByte().right()
+        "Right<Short>" -> value.convert().toShort().right()
+        "Right<Int>" -> value.convert().toInt().right()
+        "Right<Long>" -> value.convert().toLong().right()
 
         else -> throw InternalException("Unknown type.")
     }
